cd714b5d3a5512d6343b7152ffbe21e8ec85bb45
tianyinj <kittyjiang2011@gmail.com>
2018-02-15 13:10:02 -0500

    add text collapse feature

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -2,15 +2,26 @@ const app = getApp()
 
 Page({
   data: {
-    my_reviews: [],
+    my_reviews: []
+  },
 
+  toggle_collapse(e){
+    var index = e.currentTarget.dataset.index
+    var my_reviews = this.data.my_reviews
+    my_reviews[index].collapse = !my_reviews[index].collapse
+    this.setData({my_reviews: my_reviews})
   },
 
   onLoad(item){
     var my_reviews = wx.getStorageSync('my_reviews')
     console.log(my_reviews)
     if (!my_reviews) my_reviews = []
-    this.setData({ my_reviews: my_reviews})
+
+    for (var i=0; i<my_reviews.length; i++){
+      my_reviews[i].collapse = true
+    }
+    
+    this.setData({ my_reviews: my_reviews })
 
   }
 })
