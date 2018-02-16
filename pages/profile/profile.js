dc439b973c930de479bfea17a7a547652ee7a9b5
tianyinj <kittyjiang2011@gmail.com>
2018-02-16 01:01:06 -0500

    slide delete animation

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -4,14 +4,13 @@ Page({
   data: {
     my_reviews: [],
     saved_reviews: {},
-    hasMove: true,
+    hasMove: false,
+    hasStart: false,
     startX:0,
     startY:0,
     endX: 0,
-    endX: 0,
-    angle: 0,
-
-    left:0,
+    endY: 0,
+    angle: 0
   },
 
   toggle_collapse(e){
@@ -29,7 +28,7 @@ Page({
 
     for (var i=0; i<my_reviews.length; i++){
       my_reviews[i].collapse = true
-      my_reviews[i].left = this.data.left
+      my_reviews[i].left = 0
     }
     this.setData({ my_reviews: my_reviews })
 
@@ -41,22 +40,37 @@ Page({
 
   touchstart(e){
     this.setData({startX: e.changedTouches[0].clientX,
-      startY: e.changedTouches[0].clientY, hasMove : true})
+      startY: e.changedTouches[0].clientY, hasStart : true})
   },
   touchmove(e){
-    var endX = e.changedTouches[0].clientX,
-      endY = e.changedTouches[0].clientY,
-      offset = endX - this.data.startX,
-      index = e.currentTarget.dataset.index,
-      my_reviews = this.data.my_reviews
-      this.data.left = offset;
-    this.setData({my_review: my_reviews})
-    if (this.data.startX-endX > 200 && this.data.hasMove) {
-      this.delItem(index)
-      this.setData({hasMove: false})
-    }
-    this.onLoad()
+    this.data.endX = e.changedTouches[0].clientX
+    this.data.endY = e.changedTouches[0].clientY
+    var index = e.currentTarget.dataset.index
+    var offset = this.data.endX - this.data.startX
+
+    var my_reviews = this.data.my_reviews
+    my_reviews[index].left = offset
+    this.data.hasMove = true
+    this.setData({my_reviews:my_reviews})
+    
   },
+
+  touchend(e) {
+    
+    var index = e.currentTarget.dataset.index
+
+    var my_reviews= this.data.my_reviews
+
+    if (this.data.startX - this.data.endX > 200 && this.data.hasStart && this.data.hasMove) {
+      this.delItem(index)
+      this.data.hasMove = false
+    }
+    for (var i = 0; i < my_reviews.length; i++) {
+      my_reviews[i].left = 0
+    }
+    this.setData({ my_reviews: my_reviews })
+  },
+
 
   delItem(index){
     console.log(index)
